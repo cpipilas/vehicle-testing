22b96d378b0cef1c83c021c040a2b7b96b2f3a99
copi <cpipilas@econais.com>
2018-04-11 10:52:43 +0300

    update build.sh and .travis.yml

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo $0: no argument
+    exit 1
+fi
+
+PLAT = $1
+
 cd firmware/main
-make all PLATFORM=photon APPDIR=../../particleGeoLoc
-echo $?
+make all PLATFORM=$PLAT APPDIR=../../particleGeoLoc
 cd ../../
